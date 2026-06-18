7f6460bc8938dffe4840011a7a3ff160c9040ba2
zielu652 <m.zielu2005@gmail.com>
2026-06-18 17:04:00 +0200

    Database test

---
+++ Tests/database_test.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from database import Base
+from DTO.Category import Category
+from DTO.Event import Event
+from DTO.RecurrenceRule import RecurrenceRule
+from DTO.SyncMetadata import SyncMetadata
+from DTO.UserCredentials import UserCredentials
+
+engine = create_engine("sqlite:///:memory:", echo=True)
+
+print("\n--- TWORZENIE TABEL ---")
+Base.metadata.create_all(engine)
+
+print("\n--- TEST ZAPISU I ODCZYTU ---")
+with Session(engine) as session:
+    nowa_kategoria = Category(name="Moja Testowa Kategoria")
+
+    session.add(nowa_kategoria)
+    session.commit()
+
+    pobrana_kategoria = session.query(Category).first()
+
+    print("\n--- WYNIK TESTU ---")
+    if pobrana_kategoria:
+        print("✅ SUKCES! Baza działa poprawnie.")
+        print(f"Pobrane ID z bazy: {pobrana_kategoria.id}")
+        print(f"Pobrana nazwa: {pobrana_kategoria.name}")
+        print(f"Pobrany kolor: {pobrana_kategoria.color.display_name}")
+    else:
+        print("❌ BŁĄD! Baza jest pusta, coś poszło nie tak.")
